f8e89ea3a0752514b2425d950b456434091cb0c7
Vexwe <deyvid.mota.cruz@gmail.com>
2026-01-28 15:37:54 -0300

    frontend pronto pra subir

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,13 +2,32 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full h-16 flex items-center bg-zinc-900 shadow-md px-6 relative">
-      <Link href="/" style={{ fontFamily: "var(--font-gravitas)" }} className=" text-white text-xl font-semibold tracking-wide absolute left-1/2 -translate-x-1/2">
+    <header className="w-full h-16 flex items-center bg-zinc-900 shadow-md px-4 sm:px-6 relative">
+      
+      {/* Logo */}
+      <Link
+        href="/"
+        style={{ fontFamily: "var(--font-gravitas)" }}
+        className="text-white text-lg sm:text-xl font-semibold tracking-wide 
+                   absolute left-1/2 -translate-x-1/2"
+      >
         Cineavaliation
       </Link>
-      <Link href="/addmovie"
-      className="ml-auto bg-red-600 hover:bg-red-700 text-white font-medium px-4 py-2 rounded-md transition-colors duration-200"
-      >Adicione um filme!</Link>
+
+      {/* Botão */}
+      <Link
+        href="/addmovie"
+        className="
+          ml-auto
+          bg-red-600 hover:bg-red-700 text-white font-medium
+          px-3 py-1.5 sm:px-4 sm:py-2
+          rounded-md transition-colors duration-200
+          text-sm sm:text-base
+        "
+      >
+        <span className="hidden sm:inline">Adicione um filme!</span>
+        <span className="sm:hidden">+ Filme</span>
+      </Link>
     </header>
   );
 }
